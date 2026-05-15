27fb692e98506c0db98e6a0cd650a1452be0faae
Xiyuan <xliu1@tcd.ie>
2026-05-15 12:23:32 +0100

    1h02m: Add MyFirstClass bean and retrieve it from context

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -1,13 +1,18 @@
 package com.alibou.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		var ctx = SpringApplication.run(ExampleApplication.class, args);
+		MyFirstClass myFirstClass =  ctx.getBean(MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello());
 	}
 
+	@Bean
+	public MyFirstClass myFirstClass() {
+		return new MyFirstClass();
+	}
 }
---
+++ src/main/java/com/alibou/example/MyFirstClass.java
@@ -0,0 +1,7 @@
+package com.alibou.example;
+
+public class MyFirstClass {
+    public String sayHello() {
+        return "Hello from the MyFirstClass";
+    }
+}
