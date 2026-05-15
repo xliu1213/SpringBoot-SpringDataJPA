9c052133cc9438b96fac8799a40d46d3829094ae
Xiyuan <xliu1@tcd.ie>
2026-05-15 13:45:33 +0100

    1h10m: Rename MyFirstClass bean and retrieve it by name

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfig {
     @Bean
-    public MyFirstClass myFirstClass() {
+    public MyFirstClass myFirstBean() {
         return new MyFirstClass();
     }
 }
