b9a42bf3ea12d560ad4b8d19b90903d14b67a888
Xiyuan <xliu1@tcd.ie>
2026-05-18 10:58:53 +0100

    1h21m: Use @Qualifier to inject a specific MyFirstClass bean

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -1,11 +1,19 @@
 package com.alibou.example;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
+    @Qualifier("bean1")
     public MyFirstClass myFirstBean() {
         return new MyFirstClass("First bean");
+    }
+
+    @Bean
+    @Qualifier("bean2")
+    public MyFirstClass mySecondBean() {
+        return new MyFirstClass("Second bean");
     }
 }
--- src/main/java/com/alibou/example/MyFirstService.java
+++ src/main/java/com/alibou/example/MyFirstService.java
@@ -1,11 +1,12 @@
 package com.alibou.example;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
     private final MyFirstClass myFirstClass;
 
-    public MyFirstService(MyFirstClass myFirstClass) {
+    public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
 
