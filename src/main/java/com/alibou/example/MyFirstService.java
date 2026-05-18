23fce61077a0c6c40e7504f606230843c6567c89
Xiyuan <xliu1@tcd.ie>
2026-05-18 10:51:57 +0100

    1h18m: Remove redundant @Autowired from service constructor

--- src/main/java/com/alibou/example/MyFirstService.java
+++ src/main/java/com/alibou/example/MyFirstService.java
@@ -1,12 +1,10 @@
 package com.alibou.example;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
     private final MyFirstClass myFirstClass;
 
-    @Autowired
     public MyFirstService(MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
