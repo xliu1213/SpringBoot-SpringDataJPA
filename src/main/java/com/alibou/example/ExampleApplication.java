7030315871a94a729cedae2286ed568f36c20864
Xiyuan <xliu1@tcd.ie>
2026-05-19 01:17:17 +0100

    1h37m: Clean up Environment usage and load custom properties with @Value

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -8,8 +8,8 @@ public class ExampleApplication {
 		var ctx = SpringApplication.run(ExampleApplication.class, args);
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getJavaVersion());
-		System.out.println(myFirstService.getOSName());
-		System.out.println(myFirstService.readProp());
+		System.out.println(myFirstService.getCustomProperty());
+		System.out.println(myFirstService.getCustomPropertyInt());
+		System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
 	}
 }
--- src/main/java/com/alibou/example/MyFirstService.java
+++ src/main/java/com/alibou/example/MyFirstService.java
@@ -1,37 +1,40 @@
 package com.alibou.example;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:custom.properties")
 public class MyFirstService {
-    private MyFirstClass myFirstClass;
-    private Environment environment;
+    private final MyFirstClass myFirstClass;
 
-    @Autowired
-    public void setMyFirstClass(@Qualifier("bean1") MyFirstClass myFirstClass){
+    @Value("Hello Alibou students")
+    private String customProperty;
+
+    @Value("${my.prop}")
+    private String customPropertyFromAnotherFile;
+
+    @Value("123")
+    private Integer customPropertyInt;
+
+    public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
-    }
-
-    @Autowired
-    public void setEnvironment(Environment environment){
-        this.environment = environment;
     }
 
     public String tellAStory() {
         return "the dependency is saying: " + myFirstClass.sayHello();
     }
 
-    public String getJavaVersion() {
-        return environment.getProperty("java.version");
+    public String getCustomProperty() {
+        return customProperty;
     }
 
-    public String getOSName() {
-        return environment.getProperty("os.name");
+    public String getCustomPropertyFromAnotherFile() {
+        return customPropertyFromAnotherFile;
     }
 
-    public String readProp() {
-        return environment.getProperty("my.custom.property");
+    public Integer getCustomPropertyInt() {
+        return customPropertyInt;
     }
 }
