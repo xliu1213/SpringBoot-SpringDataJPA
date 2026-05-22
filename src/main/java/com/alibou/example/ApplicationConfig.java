e98a472d13579a440eb322748f16eef74362364c
Xiyuan <xliu1@tcd.ie>
2026-05-23 00:56:24 +0100

    1h53m: Setting the active profile programmatically. Use @Profile to control which beans are available for each active profile.

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -1,15 +1,18 @@
 package com.alibou.example;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
     @Bean("bean1")
+    @Profile("dev")
     public MyFirstClass myFirstBean() {
         return new MyFirstClass("First bean");
     }
 
     @Bean
+    @Profile("test")
     public MyFirstClass mySecondBean() {
         return new MyFirstClass("Second bean");
     }
