c0d3c5dae7cf2dc54c82765461c071b2552350fe
Xiyuan <xliu1@tcd.ie>
2026-05-23 02:02:02 +0100

    1h56m: Use @Profile at class level to apply a profile to all beans in the configuration

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -4,15 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class ApplicationConfig {
     @Bean("bean1")
-    @Profile("dev")
     public MyFirstClass myFirstBean() {
         return new MyFirstClass("First bean");
     }
 
     @Bean
-    @Profile("test")
     public MyFirstClass mySecondBean() {
         return new MyFirstClass("Second bean");
     }
