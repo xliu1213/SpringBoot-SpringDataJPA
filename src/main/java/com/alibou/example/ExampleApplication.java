62abca098c85c5eb39d5018735aa530c3e339e8d
Xiyuan <xliu1@tcd.ie>
2026-07-01 12:59:07 +0100

    2h17m: Update ExampleApplication.java to use default SpringApplication.run call and remove sample files

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -1,17 +1,10 @@
 package com.alibou.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Collections;
 
 @SpringBootApplication
 public class ExampleApplication {
 	public static void main(String[] args) {
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-		var ctx = app.run(args);
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomPropertyInt());
+		SpringApplication.run(ExampleApplication.class);
 	}
 }
