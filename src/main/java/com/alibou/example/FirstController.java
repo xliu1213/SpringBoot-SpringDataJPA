e8cfafa30c8a0ea27e8b82ad108b316ff95f9619
Xiyuan <xliu1@tcd.ie>
2026-07-05 01:00:52 +0100

    3h08m: Add @RequestParam example for passing query parameters to controller method

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
-    @GetMapping("/hello")
+//    @GetMapping("/hello")
     public String sayHello() {
         return "Hello from my first controller";
     }
@@ -25,8 +25,14 @@ public class FirstController {
     }
 
     // http://localhost:8080/hello/alibou
-    @GetMapping("/hello/{user-name}")
+//    @GetMapping("/hello/{user-name}")
     public String pathVar(@PathVariable("user-name") String userName) {
         return "my value = " + userName;
+    }
+
+    // http://localhost:8080/hello?param_name = paramvalue&param_name_2=value_2
+    @GetMapping("/hello")
+    public String paramVar(@RequestParam("user-name") String userName, @RequestParam("user-lastname") String userLastname) {
+        return "my value = " + userName + " " + userLastname;
     }
 }
